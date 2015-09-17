788e60d8b045afad6a850f7b861334c3bb001f0a
Alvin <alvinlee123@hotmail.com>
2015-09-17 13:43:25 -0700

    readd scrape

--- scrape.py
+++ scrape.py
@@ -1,22 +1,25 @@
 from bs4 import BeautifulSoup as bs
 from flask import Flask, request, redirect
+import requests
+import twilio
 import twilio.twiml
+from twilio.rest import TwilioRestClient
 
 import requests
 import time
 app = Flask(__name__)
-ACCOUNT_SID = "AC6ac1b036ec6592332e66c53bca225861"
-AUTH_TOKEN = "6262d115a335b5ee58230da65d0920e8"
 
-client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
+client = TwilioRestClient()
 
 
 
 @app.route("/", methods=['GET', 'POST'])
 def search():
     """respond to incoming requests"""
-
-    symbol=request.values.get('Body', None)
+    try:
+        symbol=request.form['Body']
+    except:
+        pass
     search_url='http://www.google.com/finance?q='+symbol
     try:
         url=requests.get(search_url).text
@@ -29,7 +32,11 @@ def search():
         	body=message,
         )
     except:
-        pass
+        client.messages.create(
+        	to="5107120735",
+        	from_="+16507310668",
+        	body="The ticker symbol you sent is invalid or isn't listed on google finance",
+        )
 
 if __name__ == '__main__':
     app.run(debug=True)
